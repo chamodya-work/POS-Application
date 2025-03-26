e1a47e5f9a176e0dd9dd7095c7380d61f6d8df97
chamodya <chamodyarajapaksha1@gmail.com>
2025-03-26 21:09:03 +0530

    updated to OrderSaveDTO and OrderDetailsSaveDTO

---
+++ src/main/java/com/springbootacademy/pos/dto/request/RequestOrderDetailsSave.java
@@ -0,0 +1,15 @@
+package com.springbootacademy.pos.dto.request;
+
+import com.springbootacademy.pos.entity.Item;
+import com.springbootacademy.pos.entity.Order;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+public class RequestOrderDetailsSave {
+    private String itemName;
+    private double qty;
+    private Double amount;
+    private int items;
+    private int order;
+}
---
+++ src/main/java/com/springbootacademy/pos/dto/request/RequestOrderSaveDTO.java
@@ -0,0 +1,23 @@
+package com.springbootacademy.pos.dto.request;
+
+import com.springbootacademy.pos.entity.Customer;
+import com.springbootacademy.pos.entity.OrderDetails;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Data
+
+public class RequestOrderSaveDTO {
+    private int customer;
+    private Date date;
+    private Double total;
+    private List<RequestOrderDetailsSave> orderDetail;
+
+}
